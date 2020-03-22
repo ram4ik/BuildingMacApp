2d110ea23f7f04ee7f46e55648a70a9f3933e11d
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-03-22 20:15:58 +0200

    init

--- BuildingMacApp/ContentView.swift
+++ BuildingMacApp/ContentView.swift
@@ -8,10 +8,51 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct Question {
+    let text: String
+    let actualAnswer: String
+    var userAnswer = ""
+    
+    init() {
+        let left = Int.random(in: 1...10)
+        let right = Int.random(in: 1...10)
+        
+        text = "\(left) + \(right) = "
+        actualAnswer = "\(left + right)"
+    }
+}
+
+struct QuestionRow: View {
+    var question: Question
+    
     var body: some View {
-        Text("Hello, World!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        HStack {
+            Text("5 + 5 = ")
+                .padding([.top, .bottom, .leading])
+        }
+        .font(.system(size: 48, weight: .regular, design: .monospaced))
+        .foregroundColor(.white)
+    }
+}
+
+struct ContentView: View {
+    @State private var questions = [Question]()
+    
+    var body: some View {
+        ZStack {
+            ForEach(0..<questions.count, id: \.self) { index in
+                QuestionRow(question: self.questions[index])
+                    .offset(x: 0, y: CGFloat(index) * 100)
+            }
+        }
+        .frame(width: 1000, height: 600)
+    .onAppear(perform: createQuestions)
+    }
+    
+    func createQuestions() {
+        for _ in 1...50 {
+            questions.append(Question())
+        }
     }
 }
 
